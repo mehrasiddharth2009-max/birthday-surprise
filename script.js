7f9b31ce5be96fb42af0a8b3f3bc9c0546c9b219
mehrasiddharth2009-max <mehrasiddharth2009@gmail.com>
2026-07-11 23:21:27 +0530

    Module 2.2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const lightScreen = document.getElementById("lightScreen");
 const lightBtn = document.getElementById("lightBtn");
 const fairyLights = document.querySelector(".fairyLights");
 const title = document.getElementById("birthdayTitle");
+const giftBox = document.getElementById("giftBox");
 
 // 2 sec loading
 setTimeout(() => {
@@ -17,6 +18,12 @@ lightBtn.addEventListener("click", () => {
 
     title.style.display = "block";
 
-    lightBtn.style.display = "none";
+    lightBtn.style.display = "none";giftBox.style.display = "block";
+giftBox.addEventListener("click", () => {
+    giftBox.classList.add("open");
 
+    setTimeout(() => {
+        alert("🎁 Surprise is coming...");
+    }, 800);
+});
 });
